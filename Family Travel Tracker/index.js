7d74e99eabcab5058fa3263176e77f8fb9174923
tejas <tejas21245@gmail.com>
2025-05-07 00:59:58 +0900

    minor change

--- Family Travel Tracker/index.js
+++ Family Travel Tracker/index.js
@@ -68,8 +68,7 @@ app.post("/add", async (req, res) => {
 app.post("/user", async (req, res) => {});
 
 app.post("/new", async (req, res) => {
-  //Hint: The RETURNING keyword can return the data that was inserted.
-  //https://www.postgresql.org/docs/current/dml-returning.html
+  
 });
 
 app.listen(port, () => {
