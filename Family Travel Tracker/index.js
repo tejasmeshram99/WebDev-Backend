1de08f5dd680a4d927a2a4327bf82787edd819d1
tejas <tejas21245@gmail.com>
2025-05-07 23:41:13 +0900

    remove bogus newlines

--- Family Travel Tracker/index.js
+++ Family Travel Tracker/index.js
@@ -33,83 +33,50 @@ async function checkVisisted() {
 }
 
 app.get("/", async (req, res) => {
-
-  //Write your code here.
-
   const countries = await checkVisisted();
-
   res.render("index.ejs", { countries: countries, total: countries.length });
-
 });
 
 app.post("/add", async (req, res) => {
-
   const countryToAdd = req.body.country;
-
   var countryCode = ""
-
+  
   // Get the Code for the country received from the form
-
   var success = true;
-
   const response = await db.query("SELECT country_code FROM public.countries WHERE LOWER(country_name) LIKE '%' || $1 || '%'", [countryToAdd.toLowerCase()]);
-
   console.log(response.rows);
-
   if (response.rows.length === 1) {
-
     countryCode = response.rows[0]["country_code"];
-
   }
 
   else {
-
     const countries = await checkVisisted();
-
     var error = "Country name does not exist, try again."
-
     success = false;
-
     res.render("index.ejs", { countries: countries, total: countries.length, error: error });
-
   }
 
   // Add the code to the visited countries table
 
   if (countryCode) {
-
     try {
-
       await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [`${countryCode}`]);
-
     }
 
     catch (err) {
-
       console.log(err);
-
       const countries = await checkVisisted();
-
       var error = "Country already exists in the DB."
-
       success = false;
-
       res.render("index.ejs", { countries: countries, total: countries.length, error: error });
-
     }
-
   }
 
   if (success) {
-
     res.redirect('/');
-
   }
-
 });
 
 app.listen(port, () => {
-
   console.log(`Server running on http://localhost:${port}`);
-
 });
